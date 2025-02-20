a1c9fd6f0c7c8b52bce8da91ff310a004e55dae8
Gagan <singh.gagan144@gmail.com>
2025-02-19 21:13:38 -0800

    Project Seed

---
+++ README.md
@@ -0,0 +1,18 @@
+# Spring Leaderboard (Laravel|PHP)
+
+## Project Kickstart
+- Start Project:
+    ```shell
+    laravel new sprint-leaderboard
+    ```
+
+- Migrate
+    ```shell
+    php artisan migrate
+    ```
+
+- Migrate
+    ```shell
+    php artisan run
+    ```
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Spring Leaderboard</title>
+</head>
+<body>
+    <h1>Spring Leaderboard</h1>
+    <hr/>
+</body>
+</html>
